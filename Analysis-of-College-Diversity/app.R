1673d6c4771c7c037c2e3330d94f9a73f01b1023
emfree19 <emmafreeman@Emmas-MacBook-Pro-2.local>
2020-04-24 02:02:59 -0500

    aa section almost complete for ms 8

---
+++ Analysis-of-College-Diversity/app.R
@@ -0,0 +1,182 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(janitor)
+library(stringr)
+library(tidycensus)
+library(forcats)
+library(ggthemes)
+library(dotwhisker)
+library(broom)
+library(patchwork) 
+
+mean_data <- read_csv("mean_data.csv", col_types = cols())
+mean_data <- mean_data %>%
+    mutate(degree_of_urbanization_urban_centric_locale = 
+               factor(degree_of_urbanization_urban_centric_locale, 
+                      levels = c("Rural", "Town", "Suburb", "City")))
+
+
+ui <- fluidPage(
+    ui <- navbarPage(
+        "Diversity of Upper Level Education",
+        tabPanel("About", 
+                 titlePanel("About"),
+                 h3("Project Background and Motivations"),
+                 p("This project aims to determine the factors that correlate 
+                 with racial and ethnic diversity of college populations. I 
+                 picked several variables that I wanted to explore: size of 
+                 institution, percent of women, admissions rate, and locale. 
+                 Furthermore, I chose the top three minorities (latinx, african 
+                 american, and asian) to consider. Note, I have the information for other
+                 race data. I found the correlations between these independent variables 
+                 and the dependent variables defined above. I found the correlation 
+                 coefficients and defined the independent variables categorically 
+                 in order to more clearly show the relationship in the data. I 
+                 used these variables to help determine what may to racially 
+                 diverse universities and racially uniform universities.
+                 This was a central topic (before the pandemic) due to the recent 
+                 lawsuit regarding Harvard's admissions practices."),
+                 h3("Data"),
+                 p("To accomplish the goal of this project, I have amassed data 
+                 sets from each year (between 2010 to 2018) that contain information 
+                 about almost 7,000 institutions in the continental United States. 
+                 Each data set had the pertinent variables about these institutions for 
+                 a given year. The data was taken from the", 
+                 a(href = "https://nces.ed.gov/ipeds/use-the-data", "National Center 
+                 for Education Statistics"), 
+                 "In addition, I have loaded race data from 
+                 the tidycensus r package in order to compare the mean racial population
+                 of the country as a whole to determine the either overrepresentation or 
+                 underrepresentation of a minority"),
+                 h3("About Me"),
+                 p("My name is Emma Freeman and I study Computer Science at Harvard. 
+                    You can reach me at efreeman@college.harvard.edu."),
+                 p("The code for this project can be found at",
+                   a(href = "https://github.com/emfree19/final_project", "github repo"), ".")),
+        tabPanel("Latinx Student Population"),
+        tabPanel("African American Student Population",
+                 fluidPage(
+                     sidebarPanel(
+                         width = 6,
+                         selectInput(
+                             "plot_type",
+                             "Independent Variables",
+                             c("Size of Institution, Percent Women, Admissions Rate" = "a", 
+                               "Locale" = "b")),
+                         p("Here are the coefficients."),
+                         plotOutput("aa_coefficients")
+                     ),
+                     mainPanel(width = 6, 
+                               plotOutput("aa_institution"),
+                               plotOutput("aa_women"),
+                               plotOutput("aa_admissions"))
+                 )),
+        tabPanel("Asian Student Population")
+        )
+)
+        
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    output$aa_coefficients <- renderPlot({
+        if(input$plot_type == "a") {
+            model_african_american <- lm(mean_african_american ~ mean_enrollment + 
+                                             mean_women + 
+                                             mean_admitted,
+                                         data = mean_data) %>%
+                tidy(conf.int = TRUE)
+            dwplot(model_african_american) %>%
+                relabel_predictors(mean_enrollment = "Undergraduate Enrollment",
+                                   mean_women = "Percentage of Women",
+                                   mean_admitted = "Admissions Rate") +
+                theme_classic() +
+                ggtitle("Correlation between Independent Variables \n & African American Student Population") +
+                theme(legend.position = "none")
+        }
+        else{
+            if(input$plot_type == "b") {
+                model_african_american_2 <- lm(mean_african_american ~ degree_of_urbanization_urban_centric_locale,
+                                               data = mean_data) %>%
+                    tidy(conf.int = TRUE)
+                
+                dwplot(model_african_american_2, show_intercept = TRUE) %>%
+                    relabel_predictors(`(Intercept)` = "Baseline: Rural",
+                                       degree_of_urbanization_urban_centric_localeTown = "Town",
+                                       degree_of_urbanization_urban_centric_localeSuburb = "Suburb",
+                                       degree_of_urbanization_urban_centric_localeCity = "City") +
+                    theme_classic() +
+                    ggtitle("Correlation between Locale \n & African American Student Population") +
+                    theme(legend.position = "none")
+            }
+        }
+         
+    })
+    output$aa_institution <- renderPlot({
+        if(input$plot_type == "a") {
+            ggplot(mean_data, aes(x = institution_size_category, y = mean_african_american)) +
+                geom_boxplot(alpha = 0.25) +
+                xlab("Size of Institution") + 
+                ylab("Percent Black or African American") +
+                theme_classic() + 
+                labs(title = "Percent of Black or African Students by Size of Institution",
+                     subtitle = "With comparison at national population estimate") +
+                geom_hline(yintercept = census_data_2$pct_african_american, color = "red")
+        }
+        else{
+            if(input$plot_type == "b") {
+                ggplot(mean_data, aes(x = as.factor(degree_of_urbanization_urban_centric_locale), y = mean_african_american)) +
+                    geom_boxplot(alpha = 0.25) +
+                    xlab("Level of Urbanization of the Institution") + 
+                    ylab("Percent Black or African American") +
+                    theme_classic() + 
+                    labs(title = "Percent of Black or African Students by Level of Urbanization",
+                         subtitle = "With comparison at national population estimate") +
+                    geom_hline(yintercept = census_data_2$pct_african_american, color = "red")
+            }
+        }
+        
+    })
+    output$aa_women <- renderPlot({
+        if(input$plot_type == "a") {
+            ggplot(mean_data, aes(x = as.factor(quantiles_women), y = mean_african_american)) +
+                geom_boxplot(alpha = 0.25) +
+                xlab("Percent of Female Students") + 
+                ylab("Percent Black or African American Students") +
+                theme_classic() + 
+                labs(title = "Percent of Black or African Students by Percent of Female Students",
+                     subtitle = "With comparison at national population estimate") +
+                geom_hline(yintercept = census_data_2$pct_african_american, color = "red")
+        }
+        else{}
+    })
+    
+    output$aa_admissions <- renderPlot ({ 
+        if(input$plot_type == "a") {
+            ggplot(mean_data, aes(x = as.factor(quantiles_admission), y = mean_african_american)) +
+                geom_boxplot(alpha = 0.25) +
+                xlab("Admissions Rate") + 
+                ylab("Percent Black or African American Students") +
+                theme_classic() + 
+                labs(title = "Percent of Black or African Students by Admissions Rate",
+                     subtitle = "With comparison at national population estimate") +
+                geom_hline(yintercept = census_data_2$pct_african_american, color = "red")
+        }
+        else{}
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
